0ce353e44733b044de7b56503060fa691992a3c9
Jeremy Wu <jeremy.wu.961@gmail.com>
2023-03-14 18:28:58 -0400

    Reduced script to only 21 lines by removing comments.
    Might add a seperate file to explain
    how the program works in the future.

--- Basic_Checklist.py
+++ Basic_Checklist.py
@@ -1,26 +1,5 @@
 from tkinter import *
 
-'''#open a text file with items for checklist
-fileobject = open("input_file.txt")
-
-#initialize an empty list
-dailyTasks = []
-
-#for loop to print lines of text file and append lines to dailyTasks list
-for line in fileobject:
-    dailyTasks.append(line)
-
-# close file
-fileobject.close()
-
-# make a second empty list
-dailyTasks2 = []
-
-#removes last character from "/n" and puts elements in a different list
-for task in dailyTasks:
-    task = task[:-1]
-    dailyTasks2.append(task)'''
-
 with open("input_file.txt") as fileObject:
     dailyTasks = [line.strip() for line in fileObject]
 
@@ -33,12 +12,6 @@ var.set("Daily TaskList")
 checklistTitle_Label.config(font=("Courier",35))
 checklistTitle_Label.grid(row=0,padx=20, pady=20)
 
-'''
-padx=
-Optional horizontal padding to place around the widget in a cell. Default is 0.
-pady=
-Optional vertical padding to place around the widget in a cell. Default is 0.
-'''
 r = 1
 for item in dailyTasks:
     itemNumber = str(r) + ". "
